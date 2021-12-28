1217de2dd013e659d10b14674fbc53341b2149d4
Damar Saputra <damarsaputra2626@gmail.com>
2021-12-29 03:26:57 +0700

    Module 5

---
+++ ClientProxyForm-P5-20190140100/ServiceReference1/MatematikaClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceReference1
+{
+    internal class MatematikaClient
+    {
+        public MatematikaClient()
+        {
+        }
+
+        internal double Tambah(int angka1, int angka2)
+        {
+            return 0;
+        }
+
+        internal double Kali(int angka1, int angka2)
+        {
+            return 0;
+        }
+
+        internal double Kurang(int angka1, int angka2)
+        {
+            return 0;
+        }
+
+        internal double Bagi(int angka1, int angka2)
+        {
+            return 0;
+        }
+    }
+}
